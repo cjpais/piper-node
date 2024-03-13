0e285f45d2e5699ed1c33da5ddd33d62f58dd309
CJ Pais <cj@cjpais.com>
2024-03-13 13:05:41 -0700

    attempt to use bun.spawn instead of child_process

--- src/piper.ts
+++ src/piper.ts
@@ -1,5 +1,6 @@
+import { spawn } from "bun";
 import type { OutputFormat, SpeakParams } from "./api";
-import { spawn } from "child_process";
+// import { spawn } from "child_process";
 
 const getCodec = (format: OutputFormat) => {
   switch (format) {
@@ -12,111 +13,174 @@ const getCodec = (format: OutputFormat) => {
   }
 };
 
-export const generatePiperSpeech = ({
+export const generatePiperSpeech = async ({
   model,
   speed,
   sentenceSilence,
   text,
   speaker,
   format,
-}: SpeakParams): ReadableStream => {
+}: SpeakParams): Promise<ReadableStream> => {
   const startTime = Date.now();
-  let chunkSent = false;
 
-  let outputProcess = spawn("piper", [
-    "--model",
-    `${process.env.MODEL_PATH}/${model}.onnx`,
-    "--output-raw",
-    "--length_scale",
-    `${speed}`,
-    "--sentence-silence",
-    `${sentenceSilence}`,
-    "--json-input",
-  ]);
+  const piperProcess = spawn(
+    [
+      "piper",
+      "--model",
+      `${process.env.MODEL_PATH}/${model}.onnx`,
+      "--output-raw",
+      "--length_scale",
+      `${speed}`,
+      "--sentence-silence",
+      `${sentenceSilence}`,
+      "--json-input",
+    ],
+    { stdin: Buffer.from(JSON.stringify({ text, speaker })) }
+  );
 
-  const input = JSON.stringify({ text, speaker });
-
-  outputProcess.stdin.write(input);
-  outputProcess.stdin.end();
+  let stream = piperProcess.stdout;
 
   if (format !== "pcm") {
-    // console.log("Using ffmpeg to convert to", format);
-    const ffmpegProcess = spawn("ffmpeg", [
-      "-f",
-      "s16le",
-      "-ar",
-      "22050",
-      "-ac",
-      "1",
-      "-i",
-      "-",
-      "-acodec",
-      getCodec(format),
-      "-q:a",
-      "2",
-      "-f",
-      format,
-      "-",
-    ]);
-
-    outputProcess.stdout.pipe(ffmpegProcess.stdin);
-    outputProcess = ffmpegProcess;
+    const ffmpegProcess = spawn(
+      [
+        "ffmpeg",
+        "-f",
+        "s16le",
+        "-ar",
+        "22050",
+        "-ac",
+        "1",
+        "-i",
+        "-",
+        "-acodec",
+        getCodec(format),
+        "-q:a",
+        "2",
+        "-f",
+        format,
+        "-",
+      ],
+      {
+        stdin: piperProcess.stdout,
+      }
+    );
   }
 
-  let isControllerClosed = false; // Flag to track if the controller has been closed
+  const [s1, s2] = stream.tee();
 
-  const webStream = new ReadableStream({
-    start(controller) {
-      outputProcess.stdout.on("data", (chunk) => {
-        if (!chunkSent) {
-          console.log("Took", Date.now() - startTime, "ms for the first chunk");
-          chunkSent = true;
-        }
-        controller.enqueue(new Uint8Array(chunk));
-      });
-      outputProcess.stdout.on("end", () => {
-        if (!isControllerClosed) {
-          controller.close();
-          isControllerClosed = true; // Set flag to indicate the controller is closed
-          // console.log(
-          //   "Took",
-          //   Date.now() - startTime,
-          //   "ms for the stream to end"
-          // );
-        }
-      });
-      outputProcess.stderr.on("data", (data) => {
-        // console.error(`ffmpeg stderr: ${data}`);
-      });
-      outputProcess.on("error", (err) => {
-        console.error(`ffmpeg process error: ${err}`);
-        if (!isControllerClosed) {
-          controller.error(err); // Properly signal an error to the stream
-          isControllerClosed = true; // Set flag to indicate the controller is closed
-        }
-      });
-      outputProcess.on("close", (code) => {
-        if (code !== 0) {
-          console.error(`ffmpeg process exited with code ${code}`);
-          if (!isControllerClosed) {
-            controller.error(
-              new Error(`ffmpeg process exited with code ${code}`)
-            );
-            isControllerClosed = true; // Set flag to indicate the controller is closed
-          }
-        } else {
-          if (!isControllerClosed) {
-            controller.close(); // Only close the controller when the process exits successfully
-            isControllerClosed = true; // Set flag to indicate the controller is closed
-          }
-        }
-      });
-    },
-    cancel(reason) {
-      console.log(`Stream canceled with reason: ${reason}`);
-      outputProcess.kill(); // Ensure the ffmpeg process is terminated if the stream is canceled
-    },
-  });
+  for await (const chunk of s2) {
+    console.log("time to first chunk: ", Date.now() - startTime, "ms");
+    break;
+  }
 
-  return webStream;
+  return s1;
 };
+
+// export const generatePiperSpeech = ({
+//   model,
+//   speed,
+//   sentenceSilence,
+//   text,
+//   speaker,
+//   format,
+// }: SpeakParams): ReadableStream => {
+//   const startTime = Date.now();
+//   let chunkSent = false;
+
+//   let outputProcess = spawn("piper", [
+//     "--model",
+//     `${process.env.MODEL_PATH}/${model}.onnx`,
+//     "--output-raw",
+//     "--length_scale",
+//     `${speed}`,
+//     "--sentence-silence",
+//     `${sentenceSilence}`,
+//     "--json-input",
+//   ]);
+
+//   const input = JSON.stringify({ text, speaker });
+
+//   outputProcess.stdin.write(input);
+//   outputProcess.stdin.end();
+
+//   if (format !== "pcm") {
+//     // console.log("Using ffmpeg to convert to", format);
+//     const ffmpegProcess = spawn("ffmpeg", [
+//       "-f",
+//       "s16le",
+//       "-ar",
+//       "22050",
+//       "-ac",
+//       "1",
+//       "-i",
+//       "-",
+//       "-acodec",
+//       getCodec(format),
+//       "-q:a",
+//       "2",
+//       "-f",
+//       format,
+//       "-",
+//     ]);
+
+//     outputProcess.stdout.pipe(ffmpegProcess.stdin);
+//     outputProcess = ffmpegProcess;
+//   }
+
+//   let isControllerClosed = false; // Flag to track if the controller has been closed
+
+//   const webStream = new ReadableStream({
+//     start(controller) {
+//       outputProcess.stdout.on("data", (chunk) => {
+//         if (!chunkSent) {
+//           console.log("Took", Date.now() - startTime, "ms for the first chunk");
+//           chunkSent = true;
+//         }
+//         controller.enqueue(new Uint8Array(chunk));
+//       });
+//       outputProcess.stdout.on("end", () => {
+//         if (!isControllerClosed) {
+//           controller.close();
+//           isControllerClosed = true; // Set flag to indicate the controller is closed
+//           // console.log(
+//           //   "Took",
+//           //   Date.now() - startTime,
+//           //   "ms for the stream to end"
+//           // );
+//         }
+//       });
+//       outputProcess.stderr.on("data", (data) => {
+//         // console.error(`ffmpeg stderr: ${data}`);
+//       });
+//       outputProcess.on("error", (err) => {
+//         console.error(`ffmpeg process error: ${err}`);
+//         if (!isControllerClosed) {
+//           controller.error(err); // Properly signal an error to the stream
+//           isControllerClosed = true; // Set flag to indicate the controller is closed
+//         }
+//       });
+//       outputProcess.on("close", (code) => {
+//         if (code !== 0) {
+//           console.error(`ffmpeg process exited with code ${code}`);
+//           if (!isControllerClosed) {
+//             controller.error(
+//               new Error(`ffmpeg process exited with code ${code}`)
+//             );
+//             isControllerClosed = true; // Set flag to indicate the controller is closed
+//           }
+//         } else {
+//           if (!isControllerClosed) {
+//             controller.close(); // Only close the controller when the process exits successfully
+//             isControllerClosed = true; // Set flag to indicate the controller is closed
+//           }
+//         }
+//       });
+//     },
+//     cancel(reason) {
+//       console.log(`Stream canceled with reason: ${reason}`);
+//       outputProcess.kill(); // Ensure the ffmpeg process is terminated if the stream is canceled
+//     },
+//   });
+
+//   return webStream;
+// };
