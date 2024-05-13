f1141ef1545b9559cc7d9f456878f78547136388
CJ Pais <cj@cjpais.com>
2024-05-13 10:05:47 -0700

    use secret only if provided

--- src/api/middleware.ts
+++ src/api/middleware.ts
@@ -1,4 +1,8 @@
 export const validateAuthToken = (req: Request) => {
+  if (!process.env.SECRET) {
+    return true;
+  }
+
   const authHeader = req.headers.get("Authorization");
   if (!authHeader || !authHeader.startsWith("Bearer ")) return false;
 
